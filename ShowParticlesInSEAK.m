df000ba3915156d423aba47fef95fbfa5a16ed74
JeffreyEarly <jeffrey@jeffreyearly.com>
2020-08-13 14:09:15 -0700

    side-by-side figure

--- ShowParticlesInSEAK.m
+++ ShowParticlesInSEAK.m
@@ -10,6 +10,28 @@ model = SoutheastAlaskaMercatorModel();
 x0 = 41e3*ones(20,1);
 y0 = 9e3*ones(20,1);
 
+FigureSize = [50 50 350 230];
+fig1 = figure('Units', 'points', 'Position', FigureSize);
+set(gcf, 'Color', 'w');
+fig1.PaperUnits = 'points';
+fig1.PaperPosition = FigureSize;
+fig1.PaperSize = [FigureSize(3) FigureSize(4)];
+
+kappa = 0;
+integrator = AdvectionDiffusionIntegrator(model,kappa);
+
+T = 4*86400;
+dt = 864;
+
+[t,x,y] = integrator.particleTrajectories(x0,y0,T,dt);
+
+subplot(1,2,1)
+model.plotVelocityField(0,1), hold on
+model.plotTrajectories(x,y,'LineWidth',1.5)
+axis equal
+xlim([-30 60])
+ylim([-30 90])
+
 kappa = 20;
 integrator = AdvectionDiffusionIntegrator(model,kappa);
 
@@ -18,13 +40,14 @@ dt = 864;
 
 [t,x,y] = integrator.particleTrajectories(x0,y0,T,dt);
 
-
-figure
-% plot(scale(model.obstacles,1e-3)), hold on
-model.plotVelocityField(150*86400), hold on
+subplot(1,2,2)
+model.plotVelocityField(0,1), hold on
 model.plotTrajectories(x,y,'LineWidth',1.5)
 axis equal
-% xlim([-40 80])
-% ylim([-30 90])
+xlim([-30 60])
+ylim([-30 90])
 
-% print('-dpng', '-r300', 'TrajectoriesWithDiffusion.png')
+packfig(1,2)
+
+tightfig
+print('-dpng', '-r300', 'TrajectoriesWithAndWithoutDiffusion.png')
